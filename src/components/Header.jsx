61d22849122636cf02c3720053b02aee2c947b5b
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-08-06 16:32:13 -0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() {
+    return(
+        <header>
+        <h3>Olá, me chamo <span className='nome'>Gabriel Henrique</span></h3>
+        <h1>Sou Desenvolvedor Full-Stack Júnior</h1>
+        </header>
+    )
+}
+ 
+export default Header
