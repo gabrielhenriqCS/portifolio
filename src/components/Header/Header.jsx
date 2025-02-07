47e26f636e38f59e09b48f37577740ab65fdac02
Gabriel Henrique <henriquedeveloper24@gmail.com>
2025-02-07 14:48:22 -0300

    fix: update header title for clarity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
 				<h3>
 					Olá, me chamo <span className="nome">Gabriel Henrique</span>
 				</h3>
-				<h1 className="titulo">Sou Desenvolvedor Full-Stack Júnior</h1>
+				<h1 className="titulo">Desenvolvedor Full-Stack</h1>
 				<article>
 					<p>
 						Sou Desenvolvedor Full-Stack, com conhecimentos em HTML, CSS, JavaScript, ReactJS, NodeJS, MySQL e PostgreSQL. Após minha formação técnica em Redes de Computadores, decidi fazer o serviço militar obrigatório em serviço do Exército Brasileiro, onde fiquei por 1 ano. Quando eu sai, decidi iniciar meus estudos em programação, neste período fiz um curso de Desenvolvimento Full-Stack no programa Geração Tech. Atualmente, busco minha primeira oportunidade na área, para afins de aperfeiçoamento como um bom profissional de tecnologia.
